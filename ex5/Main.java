773cc0c39be512c1c4961bc02af107a4104e1b09
Carla Oliveira <54954245+carlasoliveira@users.noreply.github.com>
2023-03-21 14:51:42 -0300

    Ex. 5 - Lista para a 1ª prova

--- ex5/Main.java
+++ ex5/Main.java
@@ -6,8 +6,8 @@ public class Main {
     }
     
     public static void exibirDisciplina(){
-    System.out.println("Disciplinas cursadas no 2º ano do curso de Engenharia de Software: ");
-    System.out.println("Algoritmos e Estruturas de Dados II; \n"
+        System.out.println("Disciplinas cursadas no 2º ano do curso de Engenharia de Software: ");
+        System.out.println("Algoritmos e Estruturas de Dados II; \n"
             + "Análise e Projeto de Sistemas; \n"
             + "Arquitetura de Computadores; \n"
             + "Banco de Dados II;\n"
