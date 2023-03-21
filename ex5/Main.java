3c45951effeeff16367e3f4ad074d09e959984ec
Carla Oliveira <54954245+carlasoliveira@users.noreply.github.com>
2023-03-21 14:51:04 -0300

    Ex. 5 - Lista para a 1ª prova [correção]

--- ex5/Main.java
+++ ex5/Main.java
@@ -2,7 +2,19 @@ package ex5;
 
 public class Main {
     public static void main(String[] args) {
-        Disciplina objeto = new Disciplina();
-        objeto.exibirDisciplina();
+        exibirDisciplina();
+    }
+    
+    public static void exibirDisciplina(){
+    System.out.println("Disciplinas cursadas no 2º ano do curso de Engenharia de Software: ");
+    System.out.println("Algoritmos e Estruturas de Dados II; \n"
+            + "Análise e Projeto de Sistemas; \n"
+            + "Arquitetura de Computadores; \n"
+            + "Banco de Dados II;\n"
+            + "Cálculo; \n"
+            + "Empreendedorismo em Tecnologia da Informação; \n"
+            + "Interação Humano-Computador; \n"
+            + "Programação Orientada a Objetos; e \n"
+            + "Projeto Integrador I.");
     }
 }
